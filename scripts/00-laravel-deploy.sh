45fb96d859f2e3a7134b99cbf8a00d822f05308a
lawiss54 <ouadah.cherif@yandex.com>
2025-12-02 04:48:00 +0100

    config to deploy in vercel

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 echo "Running composer"
-composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
-
-echo "generating application key..."
-php artisan key:generate --show
 
 echo "Caching config..."
 php artisan config:cache
@@ -13,4 +9,4 @@ echo "Caching routes..."
 php artisan route:cache
 
 echo "Running migrations..."
-php artisan migrate --force
+php artisan migrate --force 
