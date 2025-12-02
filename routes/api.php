73ac1a612a8f4067b77afd389d6aad1c13306803
lawiss54 <ouadah.cherif@yandex.com>
2025-12-02 22:23:34 +0100

    fix method call in route api

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\{TelegramWebhookController, SetUpEebHookTelegram};
 
-Route::post('/telegram/setup-webhook', [SetUpEebHookTelegram::class, 'SetUpWebhook']);
-Route::post('/telegram/webhook', [TelegramWebhookController::class, 'handle']);
+Route::get('/telegram/setup-webhook', [SetUpEebHookTelegram::class, 'SetUpWebhook']);
+Route::any('/telegram/webhook', [TelegramWebhookController::class, 'handle']);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
